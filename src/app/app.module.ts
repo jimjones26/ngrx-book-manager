383a281f382133099a2f86f3357c51d022c7d477
jimjones26 <jimjones26@gmail.com>
2017-04-05 17:21:43 -0400

    import ngrx related modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MaterialModule } from '@angular/material';
 import { RouterModule } from '@angular/router';
+import { RouterStoreModule } from '@ngrx/router-store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { DBModule } from '@ngrx/db';
+import { ComponentsModule } from './components/index';
 
-import { routes } from './routes';
+import { BookEffects } from './effects/book.effect';
+import { CollectionEffects } from './effects/collection.effect';
+import { GoogleBooksService } from 'app/services/google-books.service';
+import { BookExistsGuard } from './guards/book-exists.guard';
 
 import { AppComponent } from './app.component';
-import { GoogleBooksService } from 'app/services/google-books.service';
+
+import { routes } from './routes';
+import { reducer } from './reducers';
+import { schema } from './db';
 
 
 @NgModule({
@@ -17,10 +29,56 @@ import { GoogleBooksService } from 'app/services/google-books.service';
   imports: [
     BrowserModule,
     CommonModule,
+    ComponentsModule,
     MaterialModule,
-    RouterModule.forRoot(routes, { useHash: true })
+    RouterModule.forRoot(routes, { useHash: true }),
+
+    /**
+     * StoreModule.provideStore is imported once in the root module, accepting a reducer
+     * function or object map of reducer functions. If passed an object of
+     * reducers, combineReducers will be run creating your application
+     * meta-reducer. This returns all providers for an @ngrx/store
+     * based application.
+     */
+    StoreModule.provideStore(reducer),
+
+    /**
+     * @ngrx/router-store keeps router state up-to-date in the store and uses
+     * the store as the single source of truth for the router's state.
+     */
+    RouterStoreModule.connectRouter(),
+
+    /**
+     * Store devtools instrument the store retaining past versions of state
+     * and recalculating new states. This enables powerful time-travel
+     * debugging.
+     *
+     * To use the debugger, install the Redux Devtools extension for either
+     * Chrome or Firefox
+     *
+     * See: https://github.com/zalmoxisus/redux-devtools-extension
+     */
+    StoreDevtoolsModule.instrumentOnlyWithExtension(),
+
+    /**
+     * EffectsModule.run() sets up the effects class to be initialized
+     * immediately when the application starts.
+     *
+     * See: https://github.com/ngrx/effects/blob/master/docs/api.md#run
+     */
+    EffectsModule.run(BookEffects),
+    EffectsModule.run(CollectionEffects),
+
+    /**
+     * `provideDB` sets up @ngrx/db with the provided schema and makes the Database
+     * service available.
+     */
+    DBModule.provideDB(schema)
   ],
-  providers: [GoogleBooksService],
+  providers: [
+    BookExistsGuard,
+    GoogleBooksService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/index.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from '@angular/material';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { PipesModule } from '../pipes';
+
+
+export const COMPONENTS = [];
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    RouterModule,
+    PipesModule,
+  ],
+  declarations: COMPONENTS,
+  exports: COMPONENTS
+})
+export class ComponentsModule { }
